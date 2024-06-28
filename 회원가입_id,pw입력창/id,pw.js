6915f2f58693cf16e6c364ec6d9e4cf536529e6b
TaekyeongOhMe <161999984+TaekyeongOh@users.noreply.github.com>
2024-06-28 22:41:33 +0900

    회원가입

--- 회원가입_id,pw입력창/id,pw.js
+++ 회원가입_id,pw입력창/id,pw.js
@@ -3,23 +3,18 @@
 const loginId = document.getElementById('name_input');
 const email = document.getElementById('email_input');
 const loginPw = document.getElementById('password_input');
+const loginPwCheck = document.getElementById('password_check_input');
 const nextBtn = document.getElementById('next_btn');
 
 function highlightBorder(event) {
     loginId.style.borderColor = "";
     email.style.borderColor = "";
     loginPw.style.borderColor = "";
+    loginPwCheck.style.borderColor = "";
     
     event.target.style.borderColor = "#7D08D9";
 }
 
-function enableButton() {
-    if (loginId.value.length > 0 || email.value.length > 0 || loginPw.value.length > 0) {
-        nextBtn.disabled = false;
-    } else {
-        nextBtn.disabled = true;
-    }
-}
 
 function moveToMain() {
     location.replace("./main.html");
@@ -28,9 +23,6 @@ function moveToMain() {
 loginId.addEventListener('focus', highlightBorder);
 email.addEventListener('focus', highlightBorder);
 loginPw.addEventListener('focus', highlightBorder);
-
-loginId.addEventListener('keyup', enableButton);
-email.addEventListener('keyup', enableButton);
-loginPw.addEventListener('keyup', enableButton);
+loginPwCheck.addEventListener('focus', highlightBorder);
 
 nextBtn.addEventListener('click', moveToMain);
